c797a76aed14d1697a30b951b2f7c9385f3a4908
Wes Waligora <wsw3877@ad.rit.edu>
2016-01-29 21:44:56 -0500

    Added player movement Added camera zooming Camera can't go too close or too far

--- GGJ 2016/Assets/Scripts/PlayerController.cs
+++ GGJ 2016/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float sensitivityY;
     [SerializeField]
+    float scrollSpeed;
+    [SerializeField]
     float fastTurnLimit;
+    [SerializeField]
+    float playerSpeed;
+    [SerializeField]
+    Vector3 cameraPos;
+    [SerializeField]
+    float cameraCloseLimit;
+    [SerializeField]
+    float cameraFarLimit;
 
     Transform tpsCamera;
+
 	// Use this for initialization
 	void Start ()
     {
         tpsCamera = transform.GetChild(0);
+        tpsCamera.position = cameraPos;
+        tpsCamera.LookAt(transform);
 	}
 	
 	// Update is called once per frame
@@ -25,12 +38,26 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float xMovement = Input.GetAxis("Mouse X") * sensitivityX;
+        float xMovement = Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+        float yMovement = Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
         transform.Rotate(Vector3.up, xMovement);
-        tpsCamera.RotateAround(transform.position, -transform.right, Input.GetAxis("Mouse Y") * sensitivityY);
-        if (Mathf.Abs(xMovement) > fastTurnLimit)
+        tpsCamera.RotateAround(transform.position, -transform.right, yMovement);
+        tpsCamera.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, tpsCamera.position) < cameraCloseLimit)
+        {
+            tpsCamera.Translate(0, 0, Vector3.Distance(transform.position, tpsCamera.position) - cameraCloseLimit);
+        }
+        if (Vector3.Distance(transform.position, tpsCamera.position) > cameraFarLimit)
+        {
+            tpsCamera.Translate(0, 0, Vector3.Distance(transform.position, tpsCamera.position) - cameraFarLimit);
+        }
+
+        if (Mathf.Abs(xMovement) > fastTurnLimit * Time.deltaTime)
         {
             Debug.Log("Turning too fast");
         }
+
+        transform.Translate(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
     }
 }
