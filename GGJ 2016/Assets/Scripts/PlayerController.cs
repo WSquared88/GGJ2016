b9e6041662c476529212c78dffa0f29bf6dd06de
Westley Waligora (RIT Student) <wsw3877@ad.rit.edu>
2016-01-29 20:27:18 -0500

    This commit has all of the packages that I brought in and probably won't end up using. Added a camera that rotates in the two dimensions around the player. The player rotates to the left and right. Made a player prefab

---
+++ GGJ 2016/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivityX;
+    [SerializeField]
+    float sensitivityY;
+    [SerializeField]
+    float fastTurnLimit;
+
+    Transform tpsCamera;
+	// Use this for initialization
+	void Start ()
+    {
+        tpsCamera = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void FixedUpdate()
+    {
+        float xMovement = Input.GetAxis("Mouse X") * sensitivityX;
+        transform.Rotate(Vector3.up, xMovement);
+        tpsCamera.RotateAround(transform.position, -transform.right, Input.GetAxis("Mouse Y") * sensitivityY);
+        if (Mathf.Abs(xMovement) > fastTurnLimit)
+        {
+            Debug.Log("Turning too fast");
+        }
+    }
+}
