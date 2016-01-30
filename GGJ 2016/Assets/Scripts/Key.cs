1973f9dbfc9818b40d007274fec096af14424979
Wes Waligora <wsw3877@ad.rit.edu>
2016-01-30 13:17:22 -0500

    Added an empty key class

---
+++ GGJ 2016/Assets/Scripts/Key.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Key : InteractableObject
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    protected override void Trigger(Collider player)
+    {
+        throw new NotImplementedException();
+    }
+}
