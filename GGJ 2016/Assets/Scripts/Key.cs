dbf342398bf5d6f7217639e9edb676c78aba2dc9
Wes Waligora <wsw3877@ad.rit.edu>
2016-01-30 18:58:20 -0500

    Created the spawner It spawns monsters Made the player go through objects when it picks up the key

--- GGJ 2016/Assets/Scripts/Key.cs
+++ GGJ 2016/Assets/Scripts/Key.cs
@@ -23,7 +23,12 @@ public class Key : InteractableObject
             ActivationObject obj = objectsToActivate[i].GetComponent<ActivationObject>();
             if (obj)
             {
-                obj.activate();
+                //obj.activate();
+                Collider objCollider = objectsToActivate[i].GetComponent<Collider>();
+                if (objCollider)
+                {
+                    Physics.IgnoreCollision(player, objectsToActivate[i].GetComponent<Collider>());
+                }
                 if (isActiveAndEnabled)
                 {
                     Destroy(gameObject);
